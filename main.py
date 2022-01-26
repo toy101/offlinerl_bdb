ca19cf59ea138c8cf508c83fd24b7ff619bdb488
toy101 <toy101dev@gmail.com>
2022-01-26 13:22:17 +0900

    ベータのログ用のコード

--- main.py
+++ main.py
@@ -12,26 +12,31 @@ import TD3_GAN
 # Runs policy for X episodes and returns average reward
 # A fixed seed is used for the eval environment
 def eval_policy(policy, env_name, seed, mean, std, seed_offset=100, eval_episodes=10):
-    eval_env = gym.make(env_name)
-    eval_env.seed(seed + seed_offset)
 
-    avg_reward = 0.
-    for _ in range(eval_episodes):
-        state, done = eval_env.reset(), False
-        while not done:
-            state = (np.array(state).reshape(1, -1) - mean) / std
-            action = policy.select_action(state)
-            state, reward, done, _ = eval_env.step(action)
-            avg_reward += reward
+    # Save average β
 
-    avg_reward /= eval_episodes
-    d4rl_score = eval_env.get_normalized_score(avg_reward)
+    # eval_env = gym.make(env_name)
+    # eval_env.seed(seed + seed_offset)
+    #
+    # avg_reward = 0.
+    # for _ in range(eval_episodes):
+    #     state, done = eval_env.reset(), False
+    #     while not done:
+    #         state = (np.array(state).reshape(1, -1) - mean) / std
+    #         action = policy.select_action(state)
+    #         state, reward, done, _ = eval_env.step(action)
+    #         avg_reward += reward
+    #
+    # avg_reward /= eval_episodes
+    # d4rl_score = eval_env.get_normalized_score(avg_reward)
+
+    logging = policy.get_log()
 
     print("---------------------------------------")
-    print(f"Evaluation over {eval_episodes} episodes: {d4rl_score:.3f}")
-    print(policy.get_log())
+    # print(f"Evaluation over {eval_episodes} episodes: {d4rl_score:.3f}")
+    print(logging)
     print("---------------------------------------")
-    return d4rl_score
+    return logging[5][1]
 
 
 if __name__ == "__main__":
@@ -60,7 +65,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # file_name = f"beta={args.beta}/{args.policy}_{args.env}_{args.seed}"
-    file_name = f"beta=unc/{args.policy}_{args.env}_{args.seed}"
+    file_name = f"{args.policy}_{args.env}_{args.seed}"
     print("---------------------------------------")
     print(f"Policy: {args.policy}, Env: {args.env}, Seed: {args.seed}")
     print("---------------------------------------")
@@ -119,5 +124,5 @@ if __name__ == "__main__":
         if (t + 1) % args.eval_freq == 0:
             print(f"Time steps: {t + 1}")
             evaluations.append(eval_policy(policy, args.env, args.seed, mean, std))
-            np.save(f"./results/{file_name}", evaluations)
+            np.save(f"./beta_logging/{file_name}", evaluations)
             if args.save_model: policy.save(f"./models/{file_name}")
